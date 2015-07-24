2e0534e99a2b6ed7669e6b57797a6f3d44526af6
Joerg Heintz <jheintz@illinois.edu>
2015-07-23 22:29:06 -0500

    update

--- Utilities.R
+++ Utilities.R
@@ -3,14 +3,14 @@ library(RISmed)
 print("Start PubmedSearch")
     con <-file("sequencing_methods.csv")
     data <- read.csv(con)[,3]
-n=0
+n=21
 for (i in seq_along(data)) {
     n=n+1
     #print (paste('count', i, data[n]))
-    if (n<11) {
+    if (n<31) {
         pubmed_search<-dget("pubmed_search.R")
         pubmed_search(data[n])
-        #print (data[n])
+        print (paste('id: ', i, 'query: ', (data[n])))
     }
 }
 print("End Utilities")
