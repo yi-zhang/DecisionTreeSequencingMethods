be2d3d68d31e8832f86c0f3be5f22965d73c21bb
Joerg Heintz <jheintz@illinois.edu>
2015-07-24 06:35:38 -0500

    update

--- main.R
+++ main.R
@@ -3,12 +3,12 @@ library(RISmed)
 print("start Pubmed search")
     con <-file("sequencing_methods.csv")
     data <- read.csv(con)[,3]
-n=3
+n=3  # starting point, e.g. 3 row in the csv file.
 for (i in seq_along(data)) {
     n=n+1
     print (paste('id: ', n, 'query: ', (data[n])))
     #print (paste('count', i, data[n]))
-    if (n<6) {
+    if (n<6) {  # termination point, e.g. row 5 
         pubmed_search<-dget("pubmed_datafetch.R")
         pubmed_search(data[n]) 
     }
